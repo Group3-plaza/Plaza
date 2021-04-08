e4ceb37cea6e3d01562d00ea014e212eaf1ee845
Naqeeb Rehman <rnqeeb@yahoo.com>
2021-04-08 18:35:30 -0400

    Fleshed out states and more properties

--- react-starter-main/src/Canvas.js
+++ react-starter-main/src/Canvas.js
@@ -1,8 +1,20 @@
 // Canvas that will display current pixel data.
-function Canvas(props) {
-    return (
-        <canvas id="canvas" width="500" height="500">
+import "./Canvas.css";
+import { useState, useRef, useEffect } from "react";
+import io from "socket.io-client";
 
+function Canvas(props) {
+
+    const [enabled, setEnabled] = useState(false);
+    const [data, setData] = useState([]);
+
+    function onCanvasClick() {
+        alert("Canvas was clicked")
+    }
+
+    return (
+        <canvas id="canvas" width="500" height="500" onCanvasClick={onCanvasClick}>
+            click me!
         </canvas>
     );
 }
