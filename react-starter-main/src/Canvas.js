5a08179702d36c4420c6920c9fdd810c27f30a15
Naqeeb Rehman <rnqeeb@yahoo.com>
2021-04-08 20:16:06 -0400

    Styling and experimenting with drawing on canvas.

--- react-starter-main/src/Canvas.js
+++ react-starter-main/src/Canvas.js
@@ -3,17 +3,29 @@ import "./Canvas.css";
 import { useState, useRef, useEffect } from "react";
 import io from "socket.io-client";
 
-function Canvas(props) {
+export function Canvas(props) {
 
     const [enabled, setEnabled] = useState(false);
     const [data, setData] = useState([]);
 
     function onCanvasClick() {
-        alert("Canvas was clicked")
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+        context.clearRect(0,0,canvas.width, canvas.height);
+        context.font = "15px Consolas";
+        context.fillText("Canvas was clicked", 180,250);
     }
 
+    // draw text indicating that this is the canvas element
+    window.addEventListener('load', () => {
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+        context.font = "15px Consolas";
+        context.fillText("Canvas Element", 200,250);
+    });
+
     return (
-        <canvas id="canvas" width="500" height="500" onCanvasClick={onCanvasClick}>
+        <canvas id="canvas" width="500" height="500" onClick={onCanvasClick}>
             click me!
         </canvas>
     );
