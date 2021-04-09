3474eaf90fbb1c825df2a60fc463b918b1dd171c
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-04-09 21:24:02 +0000

    ReadState() is working.

--- react-starter-main/CanvasState.py
+++ react-starter-main/CanvasState.py
@@ -1,7 +1,15 @@
 import struct
 BoardSize = 6 #board is nxn
-CurrentCanvasState = bytearray([12 for i in range(BoardSize*BoardSize)])#default number. Just means all white. 
+CurrentCanvasState = bytearray([0 for i in range(BoardSize*BoardSize)])#default number. Just means all white. 
 def readState():
+    Read_Bin = open("CanvasBin", "rb")
+    Bin_line = Read_Bin.readline()
+    for i in range(len(Bin_line)):
+        print(Bin_line[i],end=" ")
+        CurrentCanvasState[i] = Bin_line[i]
+    print(CurrentCanvasState)
+    Read_Bin.close()
+    
     #do something
     print("z")
 def getState():
@@ -18,12 +26,12 @@ def writeState():
     #write something
     print("ree")
 
-writeState()
+#writeState()
 checker = open("CanvasBin", "rb")
 gaze = checker.readlines()
 #Test below for the write stage.
-for i in range(len(gaze)):
-    print(gaze[i])
-    print(struct.unpack("{}B".format(BoardSize*BoardSize),gaze[i]))
+
+#if we need to unpack print(struct.unpack("{}B".format(BoardSize*BoardSize),gaze[i]))
 #CurrentCanvasState[1] = 4
 print(CurrentCanvasState)
+readState()
