a1ec6485d2ff99827b972b6eebe49eeb4c80ef21
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-04-09 19:43:54 +0000

    Altered some code. Made the array singular, and did some checking.

--- react-starter-main/CanvasState.py
+++ react-starter-main/CanvasState.py
@@ -1,6 +1,6 @@
 import struct
 BoardSize = 6 #board is nxn
-CurrentCanvasState = [bytearray([12 for i in range(BoardSize)]) for i in range(BoardSize)]#default number. Just means all white. 
+CurrentCanvasState = bytearray([12 for i in range(BoardSize*BoardSize)])#default number. Just means all white. 
 def readState():
     #do something
     print("z")
@@ -12,19 +12,18 @@ def setPixel(time, pixel, color):
     print("d")
 def writeState():
     bin_file = open("CanvasBin", "wb") #unsure if we should replace the entire file or just append to the end.
-    for i in range(BoardSize):
-        bin_file.write(CurrentCanvasState[i])
-        bin_file.write(bytes("\n","utf-8")) #can be removed if we want it all in one line. 
+    
+    bin_file.write(CurrentCanvasState)
     bin_file.close()
     #write something
     print("ree")
 
-for i in range(BoardSize):
-    print(CurrentCanvasState[i])
 writeState()
 checker = open("CanvasBin", "rb")
 gaze = checker.readlines()
 #Test below for the write stage.
 for i in range(len(gaze)):
     print(gaze[i])
-    print(struct.unpack("{}B".format(BoardSize+1),gaze[i]))
+    print(struct.unpack("{}B".format(BoardSize*BoardSize),gaze[i]))
+#CurrentCanvasState[1] = 4
+print(CurrentCanvasState)
