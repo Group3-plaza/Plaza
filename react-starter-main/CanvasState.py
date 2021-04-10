21e7dfa81b11203505fe6c1b9b13590632baa5ca
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-04-10 00:24:17 +0000

    FUnctions should be filled out. Woo

--- react-starter-main/CanvasState.py
+++ react-starter-main/CanvasState.py
@@ -1,37 +1,40 @@
 import struct
-BoardSize = 6 #board is nxn
-CurrentCanvasState = bytearray([0 for i in range(BoardSize*BoardSize)])#default number. Just means all white. 
+from datetime import datetime
+
+BoardSize = 10 #board is nxn
+CurrentCanvasState = bytearray([12 for i in range(BoardSize*BoardSize)])#default number. Just means all white. 
 def readState():
     Read_Bin = open("CanvasBin", "rb")
     Bin_line = Read_Bin.readline()
     for i in range(len(Bin_line)):
-        print(Bin_line[i],end=" ")
         CurrentCanvasState[i] = Bin_line[i]
-    print(CurrentCanvasState)
+    #print(CurrentCanvasState)
     Read_Bin.close()
     
-    #do something
-    print("z")
 def getState():
-    #get something
-    print("e")
-def setPixel(time, pixel, color):
-    #set something
-    print("d")
+    return CurrentCanvasState
+
+def setPixel(time, x_cord, y_cord, color):
+    hist_file = open("History_File","ab")
+    write_arr = bytearray([color, x_cord, y_cord, time])
+    hist_file.write(write_arr)
+    hist_file.close()
+    #print(write_arr)
+    CurrentCanvasState[x_cord+(y_cord*BoardSize)] = color
+    print(CurrentCanvasState)
+
 def writeState():
     bin_file = open("CanvasBin", "wb") #unsure if we should replace the entire file or just append to the end.
     
     bin_file.write(CurrentCanvasState)
     bin_file.close()
     #write something
-    print("ree")
 
-#writeState()
-checker = open("CanvasBin", "rb")
-gaze = checker.readlines()
+
 #Test below for the write stage.
 
 #if we need to unpack print(struct.unpack("{}B".format(BoardSize*BoardSize),gaze[i]))
 #CurrentCanvasState[1] = 4
-print(CurrentCanvasState)
-readState()
+#print(CurrentCanvasState)
+#readState()
+#print(getState())
