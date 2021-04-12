9525c771e056cecaea99d9330ccbb57dc6760938
Colton Johnson <coltonjohnson@coltons-mbp.resource.campus.njit.edu>
2021-04-11 21:04:11 -0400

    new format for color board

--- react-starter-main/src/ColorPicker.js
+++ react-starter-main/src/ColorPicker.js
@@ -4,26 +4,24 @@ import { ColorBox } from './ColorBox';
 
 export function ColorPicker(props) {
     [color, setColor] = useState(null);
+    //hex value of the colors to return 
     const colorsHex = {
-        0: null,
-        1: null,
-        2: null,
-        3: null,
-        4: null,
-        5: null,
-        6: null,
-        7: null,
-        8: null,
-        9: null,
-        10: null,
-        11: null,
-        12: null,
-        13: null
+        0: null, //
+        1: null, //
+        2: null, //
+        3: null, //
+        4: null, //
+        5: null, //
+        6: null, //
+        7: null, //
+        8: null, //
+        9: null, //
+        10: null, //
+        11: null, //
+        12: null, //
+        13: null //
     };
-    //colors to fill squares
-    const backgroundColors = [
-        
-    ]
+
     //when a user clicks on a specific color
     function onClickColor(index){
         //TODO set color to text of the color or hex value?????
@@ -34,7 +32,8 @@ export function ColorPicker(props) {
 
     return (
         //add colorboard to css styling for box to conatain colors 
-        <div className="colorboard">
+        //map through and pass hex value to ColorBox
+        <div className="color-board">
             {colors.map((item, index) => (
             <ColorBox value={item} onClick={() => onClickColor(index) } color={color}/>
             ))}
