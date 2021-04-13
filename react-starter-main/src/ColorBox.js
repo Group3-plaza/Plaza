5aaaa5440406f4a266a235b2092a14f4d643b869
Colton Johnson <coltonjohnson@coltons-mbp.resource.campus.njit.edu>
2021-04-13 02:52:21 -0400

    Finalize logic for component and add hex rgb values to colors

--- react-starter-main/src/ColorBox.js
+++ react-starter-main/src/ColorBox.js
@@ -3,8 +3,34 @@ import {useState} from 'react';
 
 export function ColorBox(props) {
     // this.props.color = color of the box 
+
+    //hex value of the colors to return 
+    const colorsHex = {
+        0: '#FF0000', //red
+        1: '#FF4500', //red-orange
+        2: '#FF8C00', //orange
+        3: '#FFA500', //yellow-orange
+        4: '#FFFF00', //yellow
+        5: '#ADFF2F', //yellow-green
+        6: '#008000', //green
+        7: '#20B2AA', //blue-green
+        8: '#0000FF', //blue
+        9: '#800080', //blue-violet
+        10: '#EE82EE', //violet 
+        11: '#9400D3', //red-violet
+        12: '#FFFFFF', //white
+        13: '#000000' //black
+    };
+
+    bColor = colorsHex[this.props.color]
+
+    const boxStyle = {
+        backgroundColor: bColor,
+        border: '4px solid black'
+    };
+
     return (
         //do inline styling for color from props 
-        <div class="box" onClick={props.onClick}></div>
+    <div style={boxStyle} onClick={props.onClick}>{this.props.value}</div>
     )
 }
--- react-starter-main/src/ColorPicker.js
+++ react-starter-main/src/ColorPicker.js
@@ -1,32 +1,34 @@
 // Color picker that allows the selection of a color to be used by Canvas.js
+import './ColorPicker.css'
 import {useState} from 'react';
 import { ColorBox } from './ColorBox';
 
 export function ColorPicker(props) {
     [color, setColor] = useState(null);
-    //hex value of the colors to return 
-    const colorsHex = {
-        0: null, //
-        1: null, //
-        2: null, //
-        3: null, //
-        4: null, //
-        5: null, //
-        6: null, //
-        7: null, //
-        8: null, //
-        9: null, //
-        10: null, //
-        11: null, //
-        12: null, //
-        13: null //
-    };
+    [isClicked, setClicked] = useState(false);
+
+    const colors = [
+        'Red', 
+        'Red-Orange', 
+        'Orange', 
+        'Yellow-Orange', 
+        'Yellow', 
+        'Yellow-Green', 
+        'Green', 
+        'Blue-Green', 
+        'Blue', 
+        'Blue-Violet', 
+        'Violet',
+        'Red-Violet',
+        'White',
+        'Black'
+    ]
 
     //when a user clicks on a specific color
     function onClickColor(index){
         //TODO set color to text of the color or hex value?????
-        setColor(colorsHex[index]);
-
+        setColor(index);
+        setClicked(true);
     }
 
 
@@ -35,7 +37,7 @@ export function ColorPicker(props) {
         //map through and pass hex value to ColorBox
         <div className="color-board">
             {colors.map((item, index) => (
-            <ColorBox value={item} onClick={() => onClickColor(index) } color={color}/>
+            <ColorBox value={item} onClick={() => onClickColor(index) } color={index} clicked={isClicked}/>
             ))}
         </div>
     )
