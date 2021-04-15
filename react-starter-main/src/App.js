71868fc0413ab5cb34dc2928d784ad4cf56efe1f
hsd23 <ec2-user@ip-172-31-83-104.ec2.internal>
2021-04-15 16:42:38 +0000

    up to date

--- react-starter-main/src/App.js
+++ react-starter-main/src/App.js
@@ -1,57 +1,63 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
 import './App.css';
-import io from "socket.io-client";
-import {useState, useEffect, useRef} from 'react';
+import io from 'socket.io-client';
+
+import {
+    React, useState, useRef, useEffect,
+} from 'react';
 
 // import project elements:
-import { Canvas } from './Canvas.js';
+// eslint-disable-next-line import/no-cycle
+import { Canvas } from './Canvas';
 import { ColorPicker } from './ColorPicker';
 import { Chat } from './Chat';
 
 export const socket = io();
 
 function App() {
+    // TODO: Conditionally render/configure elements based on whether or not user is logged-in.
 
-  // TODO: Conditionally render/configure elements based on whether or not user is logged-in.
-  
-  const [username, setUsername]=useState(null);
-  const usernameRef= useRef(null);
-  
-  function onSetUsername(){
-    if(usernameRef!==null){
-      const user = usernameRef.current.value;
-      if(user!==''){
-        setUsername(user);
-      }
-    }
-  }
-  
-  
-  return (
-    <div>
-    {username ? (
-      <div className="App">
-      My username: {username}
-        <div className="split left">
-          <div className="color"><ColorPicker/></div>
-          <div className="canvas"><Canvas username={username} /></div>
+    // Render colorpicker and chat only after canvas loads
+    // Set to useState(true) if you want to disable this for testing
+    const [isCanvasLoaded, setCanvasLoadState] = useState(false);
+
+    // set proper height of everything
+    useEffect(() => {
+        const colorPicker = document.getElementsByClassName('colorPicker');
+        const chat = document.getElementsByClassName('chat');
+
+        const horizontalElements = document.getElementsByClassName('horizontalElements');
+
+        colorPicker.height = horizontalElements.height - colorPicker.margin_top
+        - colorPicker.margin_bottom;
+
+        chat.height = horizontalElements.height - chat.margin_top;
+    });
+
+    // *** Use this to run code when the canvas loads successfully ***
+    useEffect(() => {}, [isCanvasLoaded]);
+
+    return (
+        <div className="horizontalElements">
+            {isCanvasLoaded
+                && (
+                    <div className="shadow container colorPicker">
+                        <ColorPicker />
+                    </div>
+                )}
+
+            <div className="container canvas">
+                <Canvas setCanvasLoadState={setCanvasLoadState} />
+            </div>
+
+            {isCanvasLoaded
+            && (
+                <div className="shadow container chat">
+                    <Chat username="Default User" />
+                </div>
+            )}
         </div>
-        <div className="split right">
-          <div className="chat"><Chat username={username} /></div>
-        </div>
-      </div>
-      ):(
-      <div className="App">
-        <div>
-          My username: <input ref={usernameRef} type="text"/>
-          <button onClick={onSetUsername}>Login</button>
-        </div>
-        <div className="canvas"><Canvas/></div>
-        <div className="chat"><Chat/></div>
-      </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
