8fc000682009f1990e8a3f1d03d8fbb43db6c8e8
Naqeeb Rehman <rnqeeb@yahoo.com>
2021-04-20 03:27:55 -0400

    Renamed test case

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Loading circle on app start', () => {
     render(<App />);
     const linkElement = screen.getByTestId('loading_circle');
     expect(linkElement).toBeInTheDocument();
