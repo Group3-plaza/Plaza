53436064202e7e72f7767260d2da6e372be19777
Elijah Cozier <Ecozier@hotmail.com>
2021-04-22 02:36:41 +0000

    Added columns to user table

--- models.py
+++ models.py
@@ -1,19 +1,24 @@
 ''' For use in database row models '''
-from app import db # pylint: disable=cyclic-import
+from app import db  # pylint: disable=cyclic-import
 
-class User(db.Model): # pylint: disable=too-few-public-methods
+
+class User(db.Model):  # pylint: disable=too-few-public-methods
     '''User entry'''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=False, nullable=False)
-    password= db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    auth_token = db.Column(db.String(255), unique=False, nullable=False)
+    pixel = db.Column(db.Integer, nullable=False)
+    pixel_num = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Username %r>' % self.username
 
-class Canvas(db.Model): # pylint: disable=too-few-public-methods
+
+class Canvas(db.Model):  # pylint: disable=too-few-public-methods
     '''Canvas entry'''
     id = db.Column(db.Integer, primary_key=True)
-    hours =  db.Column(db.Integer, unique=False, nullable=False)
+    hours = db.Column(db.Integer, unique=False, nullable=False)
     x_cord = db.Column(db.Integer, unique=False, nullable=False)
     y_cord = db.Column(db.Integer, unique=False, nullable=False)
     color = db.Column(db.Integer, unique=False, nullable=True)
