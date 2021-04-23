aa551abca4ca489fc82aed2fe2aad4eee9023445
Colton Johnson <coltonjohnson@coltons-mbp.resource.campus.njit.edu>
2021-04-22 22:11:12 -0400

    Set up files for tutorial

---
+++ src/TutorialPage.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import { React, useState } from 'react';
+import './TutorialPage.css';
+import plazaLogo from './graphics/Plaza.png';
+import { Canvas } from './Canvas';
+import { ColorPicker } from './ColorPicker';
+import Chat from './Chat';
+
+
+export function TutorialPage(props) {
+
+
+    return (
+
+    );
+}
