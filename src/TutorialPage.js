caefa7c214c1106de87d297f6e2c44e343833ad6
Colton Johnson <coltonjohnson@coltons-mbp.resource.campus.njit.edu>
2021-04-24 00:58:32 -0400

    Add start page for tutorial with start button + some styling

--- src/TutorialPage.js
+++ src/TutorialPage.js
@@ -4,15 +4,47 @@
 import { React, useState } from 'react';
 import './TutorialPage.css';
 import plazaLogo from './graphics/Plaza.png';
-import { Canvas } from './Canvas';
-import { ColorPicker } from './ColorPicker';
-import Chat from './Chat';
+import colorPicker from './graphics/colorpicker.png';
+import canvas from './graphics/canvas.png';
+import chat from './graphics/chat.png';
 
 
 export function TutorialPage(props) {
+    const [start, setStart] = useState(false);
+    // <button class="button">Next</button>
+
+    function onClickStart() {
+        setStart(true);
+        console.log('Start clicked')
+    }
 
 
-    return (
+    if(start){
+        return (
 
-    );
+            <div>
+                <div>
+                    <h1>WELCOME TO THE PLAZA TUTORIAL!</h1>
+                    <img class="colorpicker" src={colorPicker} />
+                    <img class='canvas' src={canvas} />
+                    <img class='chat' src={chat} />
+                </div>
+            </div>
+        );
+
+    }else{
+        return(
+            <div class='tutorialPage'>
+                <div class='startPage'>
+                    <h1>WELCOME TO THE PLAZA TUTORIAL!</h1>
+                    <h3>Click start to learn more about/how to use our intuitive app!</h3>
+                    <button class='startButton'>
+                        <img src={plazaLogo} alt='start tutorial' onClick={onClickStart}/>
+                    </button>
+                </div>
+                
+            </div>
+        );
+    }
+    
 }
