93c7c3d2628c363343f0b0eccef3df67168fc248
hsd23 <ec2-user@ip-172-31-83-104.ec2.internal>
2021-04-25 20:24:08 +0000

    updated the reqirment and css for history

--- src/History.js
+++ src/History.js
@@ -8,7 +8,7 @@ export default function History() {
             <dl>Welcome to Histroy page!</dl>
             <dl>Here you will be able to see the changes made to the canvas in gif</dl>
             <div className="gif">
-                <img src={changes} alt="changes" />
+                <img className="changes" src={changes} alt="changes" />
             </div>
         </div>
     );
