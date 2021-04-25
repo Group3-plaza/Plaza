d3ed3e71a88734b3fdf6f4aeb3a9e6238604b387
hsd23 <ec2-user@ip-172-31-83-104.ec2.internal>
2021-04-25 18:21:24 +0000

    History.js lint done

--- src/History.js
+++ src/History.js
@@ -3,13 +3,13 @@ import changes from './graphics/changes.gif';
 import './History.css';
 
 export default function History() {
-    return(
+    return (
         <div className="history">
             <dl>Welcome to Histroy page!</dl>
-            <dl>Here you'll be able to see the changes made to the canvas in gif</dl>
+            <dl>Here you will be able to see the changes made to the canvas in gif</dl>
             <div className="gif">
                 <img src={changes} alt="changes" />
             </div>
         </div>
-        );
+    );
 }
